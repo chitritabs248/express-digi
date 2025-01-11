594d02470ca182a757c31cce4736807da86bb1f0
Chitrita <chitritabs248@gmail.com>
2025-01-12 00:23:56 +0530

    add the remaining files

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import express, { json } from "express"
 import 'dotenv/config'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
 
 const port = process.env.PORT || 3000
-
+const morganFormat = ":method :url :status :response-time ms";
 // app.get("/", (req,res)=>{
 //     res.send("Hello from Chitrita!")
 // })
 
 let teaData = []
 let nextId = 1
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 app.listen(port, () => {
     console.log(`Server is running on the port: ${port}....`)
